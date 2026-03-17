81b422b4b57d53f33b4b280a32bdfa49db6bad1c
yuan <yuan@yuandeMacBook-Air.local>
2026-03-17 18:17:27 +0800

    add /api/vi prefix to all routes

--- internal/api/route.go
+++ internal/api/route.go
@@ -4,21 +4,21 @@ import "github.com/gin-gonic/gin"
 
 func NewRouter(handler *Handler) *gin.Engine {
 	r := gin.Default()
-
+	apiV1 := r.Group("/api/v1")
 	// 1. 创建
-	r.POST("/containers/start", handler.StartAlgorithm)
+	apiV1.POST("/containers/start", handler.StartAlgorithm)
 
 	// 2. 查询（管理视角：数据库）
-	r.GET("/containers", handler.ListContainers)
+	apiV1.GET("/containers", handler.ListContainers)
 
-	// 3.  查询（运行视角：K8s）
-	r.GET("/containers/runtime", handler.ListRuntimeContainers)
-	r.GET("/containers/:name/status", handler.GetContainerStatus)
+	// 3. 查询（运行视角：K8s）
+	apiV1.GET("/containers/runtime", handler.ListRuntimeContainers)
+	apiV1.GET("/containers/:name/status", handler.GetContainerStatus)
 
 	// 4. 操作（生命周期管理）
-	r.DELETE("/containers/:name", handler.DeleteContainer)
-	r.POST("/containers/:name/restart", handler.RestartContainer)
-	r.POST("/containers/:name/scale", handler.ScaleContainer)
+	apiV1.DELETE("/containers/:name", handler.DeleteContainer)
+	apiV1.POST("/containers/:name/restart", handler.RestartContainer)
+	apiV1.POST("/containers/:name/scale", handler.ScaleContainer)
 
 	return r
 }
